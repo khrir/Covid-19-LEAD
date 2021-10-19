3c44976518e8ddb0ec660d34b0db980580383350
Karllisson Henrique <karllisson.15henrique@hotmail.com>
2021-10-19 12:40:59 -0300

    Add imports

---
+++ LinearRegressionModel/main.py
@@ -0,0 +1,22 @@
+# imports
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+
+# Read the data
+
+# Selection
+
+# Create a mask for train and test
+
+# Linear regression
+
+# Coefficients
+
+# Plot
+
+# Evaluation
+
